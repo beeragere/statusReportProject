89b404067b7a5e9f89b8b749781971ab719f31f7
ShubhangaRbeeragere <shubhanga.rb@teamta.in>
2021-10-27 12:12:40 +0530

    added loader screen

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import CompanyName from "./loginPage/CompanyName";
 import LoginTemplate from "./loginPage/LoginTemplate";
+import LoadingScreen from "./loginPage/LoadingScreen";
 import { useState } from "react";
 import './App.css'
 
 const App = () => {
     let [userInput, setUserInput] = useState({username: "", password: ""});
     let [error, setError] = useState(null);
+    let [loader, setLoader] = useState(false);
 
 
     ////for fetching the data from the server
      const fetchData = async (username, password) => {
          try{
-            const data = await fetch(`http://localhost:8000/?username=${username}&password=${password}`)
+             setLoader(true);
+            const data = await fetch(`http://localhost:9000/?username=${username}&password=${password}`)
+            console.log(data);
             if(!data.ok){
                 throw new Error("Couldn't Connect To The Serever");
             }
@@ -22,6 +26,7 @@ const App = () => {
          }
          catch(error){
              setError(error.message);
+             setLoader(false);
          }
      }
 
@@ -51,6 +56,7 @@ const App = () => {
         <div className="app">
                 <CompanyName/>
                 <LoginTemplate formValidation={formValidation} updateInputs={updateInputs} userInput={userInput} error={error}/>
+                {loader&&<LoadingScreen/>} 
         </div> 
     )
 }
