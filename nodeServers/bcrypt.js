d00dbcaa4d8fb3c0636f8b141f9ca36784c02233
ShubhangaRbeeragere <shubhanga.rb@teamta.in>
2021-11-03 16:48:51 +0530

    added comments to bcrypt.js

--- nodeServers/bcrypt.js
+++ nodeServers/bcrypt.js
@@ -1,9 +1,12 @@
 const bcrypt = require("bcryptjs");
+
+//create random salt;
 let salt = bcrypt.genSaltSync(10);
+
+//hash the password and store the hash variable in database;
 let hash = bcrypt.hashSync("shubhanga", salt);
 
-// console.log(hash);
-
+//verify the password using the function compareSync();
 let check = bcrypt.compareSync("password", hash);
 console.log(check);
 
